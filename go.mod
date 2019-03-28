db63e26f866defecab8c867ea27ea55ca93506ca
Thomas (James) Perry <thomas@shaleapps.com>
2019-03-28 03:39:09 +0000

    created helper interfaces

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tjper/sqltool
+
+go 1.12
---
+++ sqltool.go
@@ -0,0 +1,42 @@
+// Package sqltool provides a set of functions, types and interfaces that
+// assist in working with SQL.
+// Created By: Thomas Perry (tjamesperry@hotmail.com)
+package sqltool
+
+import "database/sql"
+
+// Queryer is a single method interface encompassing the action of Querying a
+// datastore.
+type Queryer interface {
+	Query(SQL string, args ...interface{}) (sql.Rows, error)
+}
+
+// QueryRower is a single method interface encompasing the action of
+// QueryRowing a datastore.
+type QueryRower interface {
+	QueryRow(SQL string, args ...interface{}) sql.Row
+}
+
+// Queryest encompasses the Queryer and QueryRower interfaces.
+type Queryest interface {
+	Queryer
+	QueryRower
+}
+
+// Execer is a single method interface encompassing the action of Execing a
+// SQL statement in a datastore.
+type Execer interface {
+	Exec(SQL string, args ...interface{}) (sql.Result, error)
+}
+
+// Datastore encompasses the Queryest and Exerer interfaces.
+type DataStore interface {
+	Queryest
+	Execer
+}
+
+// Opener is a single method interface encompassing the action of Opening a
+// SQL DB connection.
+type Opener interface {
+	Open(driverName, dataSourceName string) (*sql.DB, error)
+}
