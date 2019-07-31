383427e043317687019577c6bd3def4b230a74dd
Thomas (James) Perry <thomas@shaleapps.com>
2019-07-31 11:20:05 -0600

    added helper functions and comments

--- store.go
+++ store.go
@@ -1,11 +1,16 @@
-// Package store provides a set of interfaces that assist in working with
-// data stores.
+// Package store provides a set of functions and interfaces that assist in
+// working with data stores.
 // Created By: Thomas Perry (tjamesperry@hotmail.com)
 package store
 
 import (
 	"context"
 )
+
+// The following types and functions only serve to enforce the store pattern.
+// An object should be created to represent a db resource. This object should
+// satisfy the necessary interfaces below. The object's logic should be
+// executed by passed the object as an argument to appropriate helper function.
 
 // Creater an object capable of creating.
 type Creater interface {
@@ -25,4 +30,24 @@ type Deleter interface {
 // Updater an object capable of updating.
 type Updater interface {
 	Update(context.Context) error
+}
+
+// Create calls the create method on a Creater.
+func Create(ctx context.Context, c Creater) error {
+	return c.Create(ctx)
+}
+
+// Retrieve calls the Retrieve method on a Retriever.
+func Retrieve(ctx context.Context, r Retriever) error {
+	return r.Retrieve(ctx)
+}
+
+// Delete calls the Delete method on a Deleter.
+func Delete(ctx context.Context, d Deleter) error {
+	return d.Delete(ctx)
+}
+
+// Update calls the Update method on a Updater.
+func Update(ctx context.Context, u Updater) error {
+	return u.Update(ctx)
 }
